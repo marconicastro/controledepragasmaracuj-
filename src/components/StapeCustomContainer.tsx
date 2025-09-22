1343dbef2508c6ec85ce57c7cf5f64b599b440ac
Marconi Castro <marconi.castro.mc@gmail.com>
2025-09-22 20:26:49 -0300

    vai dar certo 2

--- src/components/StapeCustomContainer.tsx
+++ src/components/StapeCustomContainer.tsx
@@ -194,7 +194,7 @@ export default function StapeCustomContainer({ gtmId = 'GTM-567XZCDX' }: StapeCu
       <script
         dangerouslySetInnerHTML={{
           __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=true;j.src="https://data.maracujazeropragas.com/7aibfbsewli.js?"+i;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','b6oj=HxxMMjw6S1NATykvKzM%2BQABFQ1tZUQIISxUQBwkUEwwXAhQHBwcUBxEZAlsLGAs%3D');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=true;j.src="https://data.maracujazeropragas.com/aqffwhuqxv.js?"+i;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','b6oj=HxxMMjw6S1NATykvKzM%2BQABFQ1tZUQIISxUQBwkUEwwXAhQHBwcUBxEZAlsLGAs%3D');
           `,
         }}
       />
